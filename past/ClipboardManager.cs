3083549614759f80be9e412c542a06af6e7268b5
Nate Peterson <ntpeters@mtu.edu>
2022-07-26 14:40:23 -0700

    Begin refactor of core clipboard logic

---
+++ past/ClipboardManager.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using past.Extensions;
+using Windows.ApplicationModel.DataTransfer;
+using System.CommandLine.Rendering;
+using System.IO;
+using System.Text.Json;
+using past.Wrappers;
+using System.Windows;
+
+namespace past
+{
+    public class ClipboardManager
+    {
+        private readonly IWinRtClipboardWrapper _winRtClipboard;
+        private readonly IWin32ClipboardWrapper _win32Clipboard;
+
+        public ClipboardManager()
+            : this(new WinRtClipboardWrapper(), new Win32ClipboardWrapper())
+        {
+        }
+
+        public ClipboardManager(IWinRtClipboardWrapper winRtClipboard, IWin32ClipboardWrapper win32Clipboard)
+        {
+            _winRtClipboard = winRtClipboard ?? throw new ArgumentNullException(nameof(winRtClipboard));
+            _win32Clipboard = win32Clipboard ?? throw new ArgumentNullException(nameof(win32Clipboard));
+        }
+
+        public async Task<string?> GetCurrentClipboardValueAsync(ContentType type, CancellationToken? cancellationToken = null)
+        {
+            // Using the Win32 clipboard API rather than the WinRt clipboard API as that
+            // one frequently throws "RPC_E_DISCONNECTED" when trying to access the current
+            // clipboard contents.
+
+            // Accessing the current clipboard must be done on an STA thread
+            var tsc = new TaskCompletionSource<string?>();
+            var staThread = new Thread(() =>
+            {
+                try
+                {
+                    string? value = null;
+                    if (type == ContentType.Text || type == ContentType.All)
+                    {
+                        if (_win32Clipboard.ContainsText(TextDataFormat.UnicodeText))
+                        {
+                            value = _win32Clipboard.GetText(TextDataFormat.UnicodeText);
+                        }
+                        else if (_win32Clipboard.ContainsText(TextDataFormat.Text))
+                        {
+                            value = _win32Clipboard.GetText(TextDataFormat.Text);
+                        }
+                    }
+                    else if ((type == ContentType.Image || type == ContentType.All) && _win32Clipboard.ContainsImage())
+                    {
+                        value = "[Unsupported Format: Image]";
+                    }
+                    else if ((type == ContentType.File || type == ContentType.All) && _win32Clipboard.ContainsFileDropList())
+                    {
+                        value = "[Unsupported Format: File Drop List]";
+                    }
+                    else if (type == ContentType.All)
+                    {
+                        var data = _win32Clipboard.GetDataObject();
+                        value = $"[Unsupported Format: {string.Join(',', data.GetFormats())}]";
+                    }
+
+                    tsc.SetResult(value);
+                }
+                catch (Exception e)
+                {
+                    tsc.SetException(e);
+                }
+            });
+
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+
+            if (!staThread.Join(millisecondsTimeout: 500))
+            {
+                throw new TimeoutException("Timeout while getting current clipboard contents.");
+            }
+
+            return await tsc.Task;
+        }
+
+        // TODO: Encapsulate return values, or split out setting as the current item
+        public async Task<(IClipboardHistoryItemWrapper Item, SetHistoryItemAsContentStatus? SetCurrentStatus)> GetClipboardHistoryItemAsync(int index, bool setCurrent, ContentType type, CancellationToken? cancellationToken = null)
+        {
+            var items = await _winRtClipboard.GetHistoryItemsAsync();
+            if (items.Status != ClipboardHistoryItemsResultStatus.Success)
+            {
+                throw new Exception($"Failed to get clipboard history. Result: {items.Status}");
+            }
+
+            // TODO: Filter on content type
+            var item = items.Items.ElementAt(index);
+
+            SetHistoryItemAsContentStatus? setContentStatus = null;
+            if (setCurrent)
+            {
+                setContentStatus = _winRtClipboard.SetHistoryItemAsContent(item);
+            }
+
+            return (item, setContentStatus);
+        }
+
+        public async Task<IEnumerable<IClipboardHistoryItemWrapper>> ListClipboardHistoryAsync(ContentType type, bool pinned, CancellationToken? cancellationToken = null)
+        {
+            var items = await _winRtClipboard.GetHistoryItemsAsync();
+            if (items.Status != ClipboardHistoryItemsResultStatus.Success)
+            {
+                throw new Exception($"Failed to get clipboard history. Result: {items.Status}");
+            }
+
+            if (items.Items.Count == 0)
+            {
+                throw new Exception("Clipboard history is empty");
+            }
+
+            IEnumerable<IClipboardHistoryItemWrapper> clipboardItems;
+
+            // TODO: Refactor out into separate method or provider
+            if (pinned)
+            {
+                // TODO: Get pinned clipboard history items
+                // Pinned item IDs can be read from: %LOCALAPPDATA%/Microsoft/Windows/Clipboard/Pinned/{GUID}/metadata.json
+                var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                var pinnedClipboardPath = Path.Combine(localAppDataPath, "Microsoft/Windows/Clipboard/Pinned");
+                var pinnedClipboardItemMetadataDirectory = Directory.EnumerateDirectories(pinnedClipboardPath).First(directoryPath => File.Exists(Path.Combine(directoryPath, "metadata.json")));
+                string? pinnedClipboardItemMetadataPath = null;
+                foreach (var directoryPath in Directory.EnumerateDirectories(pinnedClipboardPath))
+                {
+                    var metadataPath = Path.Combine(directoryPath, "metadata.json");
+                    if (File.Exists(metadataPath))
+                    {
+                        pinnedClipboardItemMetadataPath = metadataPath;
+                        break;
+                    }
+                }
+                if (string.IsNullOrWhiteSpace(pinnedClipboardItemMetadataPath))
+                {
+                    throw new Exception("Failed to retrieve pinned clipboard history items");
+                }
+
+                var pinnedClipboardItemMetadataJson = File.ReadAllText(pinnedClipboardItemMetadataPath);
+                var pinnedClipboardItemMetadata = JsonDocument.Parse(pinnedClipboardItemMetadataJson);
+                var pinnedClipboardItemIds = pinnedClipboardItemMetadata.RootElement.GetProperty("items").EnumerateObject().Select(property => property.Name);
+                clipboardItems = items.Items.Where(item => pinnedClipboardItemIds.Contains(item.Id));
+                if (clipboardItems.Count() == 0)
+                {
+                    throw new Exception("No pinned items in clipboard history");
+                }
+            }
+            else
+            {
+                clipboardItems = items.Items;
+            }
+
+            var filteredItems = clipboardItems.Where(item => type.Supports(item.Content.Contains));
+            if (filteredItems.Count() == 0)
+            {
+                throw new Exception("No supported items in clipboard history");
+            }
+
+            return filteredItems;
+        }
+    }
+}
