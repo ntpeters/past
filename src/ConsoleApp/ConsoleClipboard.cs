04c417ed665ade685ca961d637cb5ed67941910f
Nate Peterson <ntpeters@mtu.edu>
2022-07-31 19:03:03 -0700

    Fix bugs in ConsoleClipboard

--- src/ConsoleApp/ConsoleClipboard.cs
+++ src/ConsoleApp/ConsoleClipboard.cs
@@ -1,8 +1,7 @@
-using past.ConsoleApp.Extensions;
 using past.Core;
 using System;
-using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.DataTransfer;
@@ -24,30 +23,34 @@ namespace past.ConsoleApp
         }
 
         #region Public Methods
-        public void GetClipboardHistoryStatus(InvocationContext context, IConsole console, bool quiet, CancellationToken cancellationToken)
+        public void GetClipboardHistoryStatus(IConsoleWriter consoleWriter, InvocationContext context)
         {
+            _ = consoleWriter ?? throw new ArgumentNullException(nameof(consoleWriter));
+            _ = context ?? throw new ArgumentNullException(nameof(context));
+
             try
             {
-                console.WriteLine($"Clipboard History Enabled: {_clipboard.IsHistoryEnabled()}");
-                console.WriteLine($"Clipboard Roaming Enabled: {_clipboard.IsRoamingEnabled()}");
+                consoleWriter.WriteLine($"Clipboard History Enabled: {_clipboard.IsHistoryEnabled()}");
+                consoleWriter.WriteLine($"Clipboard Roaming Enabled: {_clipboard.IsRoamingEnabled()}");
+
+               context.ExitCode = 0;
             }
             catch (Exception e)
             {
-                console.WriteErrorLine($"Failed to get current clipboard history status. Error: {e}", suppressOutput: quiet);
+                consoleWriter.WriteErrorLine($"Failed to get current clipboard history status. Error: {e.Message}");
                 context.ExitCode = -1;
             }
-
-            context.ExitCode = 0;
         }
 
-        public async Task<int> GetCurrentClipboardValueAsync(IConsoleWriter consoleWriter, ContentType type, CancellationToken cancellationToken)
+        public async Task<int> GetCurrentClipboardValueAsync(IConsoleWriter consoleWriter, IValueFormatter formatter, ContentType type, CancellationToken cancellationToken)
         {
             _ = consoleWriter ?? throw new ArgumentNullException(nameof(consoleWriter));
+            _ = formatter ?? throw new ArgumentNullException(nameof(formatter));
 
             try
             {
                 string? value = await _clipboard.GetCurrentClipboardValueAsync(type, cancellationToken);
-                consoleWriter.WriteValue(value);
+                consoleWriter.WriteValue(value, formatter);
             }
             catch (Exception e)
             {
@@ -58,12 +61,16 @@ namespace past.ConsoleApp
             return 0;
         }
 
-        public async Task<int> GetClipboardHistoryItemAsync(IConsoleWriter consoleWriter, ClipboardItemIdentifier identifier, ContentType type, bool setCurrent, CancellationToken cancellationToken)
+        public async Task<int> GetClipboardHistoryItemAsync(IConsoleWriter consoleWriter, IValueFormatter formatter, ClipboardItemIdentifier identifier, ContentType type, bool setCurrent, CancellationToken cancellationToken)
         {
+            _ = consoleWriter ?? throw new ArgumentNullException(nameof(consoleWriter));
+            _ = formatter ?? throw new ArgumentNullException(nameof(formatter));
+            _ = identifier ?? throw new ArgumentNullException(nameof(identifier));
+
             try
             {
                 var (item, setContentStatus) = await _clipboard.GetClipboardHistoryItemAsync(identifier, setCurrent, type, cancellationToken);
-                await consoleWriter.WriteItemAsync(item, type);
+               await consoleWriter.WriteItemAsync(item, type, formatter: formatter);
 
                 if (setCurrent && setContentStatus != SetHistoryItemAsContentStatus.Success)
                 {
@@ -72,7 +79,7 @@ namespace past.ConsoleApp
             }
             catch (Exception e)
             {
-                consoleWriter.WriteErrorLine($"Failed to get clipboard history. Error: {e}");
+                consoleWriter.WriteErrorLine($"Failed to get clipboard history. Error: {e.Message}");
                 return -1;
             }
 
@@ -81,20 +88,23 @@ namespace past.ConsoleApp
 
         public async Task<int> ListClipboardHistoryAsync(IConsoleWriter consoleWriter, IValueFormatter formatter, ContentType type, bool pinned, CancellationToken cancellationToken)
         {
+            _ = consoleWriter ?? throw new ArgumentNullException(nameof(consoleWriter));
+            _ = formatter ?? throw new ArgumentNullException(nameof(formatter));
+
             try
             {
                 var clipboardItems = await _clipboard.ListClipboardHistoryAsync(type, pinned, cancellationToken);
 
-                int i = 0;
+                int index = 0;
                 foreach (var item in clipboardItems)
                 {
-                    await consoleWriter.WriteItemAsync(item, type, i, formatter);
-                    i++;
+                    await consoleWriter.WriteItemAsync(item, type, index, formatter, emitLineEnding: index < clipboardItems.Count() - 1);
+                    index++;
                 }
             }
             catch (Exception e)
             {
-                consoleWriter.WriteErrorLine($"Failed to get clipboard history. Error: {e}");
+                consoleWriter.WriteErrorLine($"Failed to get clipboard history. Error: {e.Message}");
                 return -1;
             }
 
