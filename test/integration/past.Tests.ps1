12e166f0bd7b400bf024ef627bda3bec0a35163a
Nate Peterson <ntpeters@mtu.edu>
2024-03-15 16:27:18 -0700

    Validate exit code for base command

--- test/integration/past.Tests.ps1
+++ test/integration/past.Tests.ps1
@@ -16,13 +16,16 @@ BeforeDiscovery {
 Describe 'past' {
     It 'Outputs current clipboard contents' {
         # Arrange
+        $expectedExitCode = 0
         $expectedContent = "This is a test!"
         Set-Clipboard $expectedContent
 
         # Act
         $actualContent = Invoke-NativeCommand -Command $past
+        $actualExitCode = $LASTEXITCODE
 
         # Assert
         $actualContent | Should -BeExactly $expectedContent
+        $actualExitCode | Should -BeExactly $expectedExitCode
     }
 }
